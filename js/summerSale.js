ece0dcebb7e2eaa37f94cb42b81077e2a632b2d4
Md Neamul Hoqe <hoqe1997@gmail.com>
2023-08-21 20:44:48 +0600

    go home button update from reset the cart to refresh the page

--- js/summerSale.js
+++ js/summerSale.js
@@ -1,51 +1,60 @@
 console.clear();
 /* set Coupon from header section */
+const couponBox = document.getElementById("coupon");
 const coupon = document.getElementById("coupon").innerText;
 
-/* get coupon from user */
+/**
+ *  Cart Variables
+ * */
+/* [get coupon from user] */
 const couponField = document.getElementById("couponField");
 const couponApplyBtn = document.getElementById("couponApplyBtn");
 const purchaseBtn = document.getElementById("purchaseBtn");
-
-const productsSide = document.getElementById("productsSide");
 const orderList = document.getElementById("orderList");
 const totalPriceElement = document.getElementById("totalPrice");
-const grandTotalElement = document.getElementById("grandTotal");
 const discountElement = document.getElementById("discount");
+const grandTotalElement = document.getElementById("grandTotal");
 
+/* Cart Controllers */
 let totalPrice = 0,
   discount = 0;
 
-productsSide.addEventListener("click", function (clickEvent) {
-  const card = clickEvent.target.closest(".card");
-  if (card) {
-    console.log(card);
-    const title = card.querySelector(".card-title").innerText;
+document
+  .getElementById("productsSide")
+  .addEventListener("click", function (clickEvent) {
+    const card = clickEvent.target.closest(".card");
+    if (card) {
+      const title = card.querySelector(".card-title").innerText;
 
-    const priceString = card.querySelector(".price").innerText;
-    const price = parseFloat(priceString);
-    totalPrice += price;
+      const priceString = card.querySelector(".price").innerText;
+      const price = parseFloat(priceString);
+      totalPrice += price;
 
-    /* button disabled/enabled */
-    if (totalPrice >= 200) {
-      couponApplyBtn.classList.remove("btn-disabled");
+      /* button disabled/enabled */
+      if (totalPrice >= 200) {
+        couponApplyBtn.classList.remove("btn-disabled");
+      }
+      if (totalPrice > 0) {
+        purchaseBtn.classList.remove("btn-disabled");
+      }
+
+      /* Cart Output Show */
+      const list = document.createElement("li");
+      orderList.appendChild(list).innerText = title;
+      totalPriceElement.innerText = totalPrice.toFixed(2);
+      grandTotalElement.innerText = (totalPrice - discount).toFixed(2);
     }
-    if (totalPrice > 0) {
-      purchaseBtn.classList.remove("btn-disabled");
-    }
+  });
 
-    /* Cart Output Show */
-    const list = document.createElement("li");
-    orderList.appendChild(list).innerText = title;
-    totalPriceElement.innerText = totalPrice.toFixed(2);
-    grandTotalElement.innerText = (totalPrice - discount).toFixed(2);
-  }
+couponBox.addEventListener("click", function () {
+  couponField.value = coupon; /* Copy to the coupon field */
+  couponBox.setAttribute("title", "Copied");
 });
 
-/* Coupon validation */
+/* Coupon verification */
 couponApplyBtn.addEventListener("click", function () {
   const userCoupon = couponField.value;
-  console.log(userCoupon);
+
   if (coupon === userCoupon) {
     discount = totalPrice * 0.2;
 
@@ -53,16 +62,13 @@ couponApplyBtn.addEventListener("click", function () {
     discountElement.innerText = discount.toFixed(2);
     grandTotalElement.innerText = (totalPrice - discount).toFixed(2);
   } else {
-    return alert("Sorry! This coupon has no discount yiet.");
+    return alert("Sorry! This coupon has no discount yet.");
   }
 });
 
 /* reset on click over "go home" */
 function resetCart() {
-  totalPriceElement.innerText = 0;
-  discountElement.innerText = 0;
-  grandTotalElement.innerText = 0;
-  orderList.innerHTML = "";
-  purchaseBtn.classList.add("btn-disabled");
-  couponApplyBtn.classList.add("btn-disabled");
+  localStorage.clear();
+  console.clear();
+  location.href = location.origin + location.pathname;
 }
